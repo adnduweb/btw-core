ed7b8b05ae239ef09d5bed7824f1701d21ccd55a
Fabrice Loru <34213953+adnduweb@users.noreply.github.com>
2023-03-06 16:50:47 +0100

    Error dir

--- src/Views/admin/permissions/table_row.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <td class="border-dashed border-t border-gray-300 p-3"><?= $permission[0]; ?></td>
-    <td class="border-dashed border-t border-gray-300 p-3"><?= $permission[1]; ?></td>
-    <td class="border-dashed border-t border-gray-300 p-3 text-end">
-        
-    </td>
-</tr>
