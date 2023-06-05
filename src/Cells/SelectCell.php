10140c2063bcba5b1bb36dfb0efdb25fb5774905
Fabrice Loru <34213953+adnduweb@users.noreply.github.com>
2023-06-05 08:17:38 +0200

    Refactoring

--- src/Cells/SelectCell.php
+++ src/Cells/SelectCell.php
@@ -17,10 +17,12 @@ class SelectCell
 {
 
     protected $xOnClick;
+    protected $class;
+    protected $label;
     protected $xChange;
     protected $hxGet;
-    protected $hxTarget; 
-    protected $hxInclude; 
+    protected $hxTarget;
+    protected $hxInclude;
     protected $hxTrigger;
     protected $hxSwap;
 
@@ -41,34 +43,37 @@ class SelectCell
             throw new RuntimeException('You must provide the Select view cell with the selected to use.');
         }
 
-        $this->xOnClick = (isset($params['xOnClick'])) ? 'x-on:click="'.$params['xOnClick'].'"' : false;
-        $this->xChange = (isset($params['change'])) ? '@change="'.$params['change'].'"' : false;
-        $this->hxGet = (isset($params['hxGet'])) ? 'hx-get="'.$params['hxGet'].'"' : false;
-        $this->hxTarget = (isset($params['hxTarget'])) ? 'hx-target="'.$params['hxTarget'].'"' : false;
-        $this->hxInclude = (isset($params['hxInclude'])) ? 'hx-include="'.$params['hxInclude'].'"' : false;
-        $this->hxTrigger = (isset($params['hxTrigger'])) ? 'hx-trigger="'.$params['hxTrigger'].'"' : false;
-        $this->hxSwap = (isset($params['hxSwap'])) ? 'hx-swap="'.$params['hxSwap'].'"' : false;
+        $this->xOnClick = (isset($params['xOnClick'])) ? 'x-on:click="' . $params['xOnClick'] . '"' : false;
+        $this->xChange = (isset($params['change'])) ? '@change="' . $params['change'] . '"' : false;
+        $this->hxGet = (isset($params['hxGet'])) ? 'hx-get="' . $params['hxGet'] . '"' : false;
+        $this->hxTarget = (isset($params['hxTarget'])) ? 'hx-target="' . $params['hxTarget'] . '"' : false;
+        $this->hxInclude = (isset($params['hxInclude'])) ? 'hx-include="' . $params['hxInclude'] . '"' : false;
+        $this->hxTrigger = (isset($params['hxTrigger'])) ? 'hx-trigger="' . $params['hxTrigger'] . '"' : false;
+        $this->hxSwap = (isset($params['hxSwap'])) ? 'hx-swap="' . $params['hxSwap'] . '"' : false;
+        $this->class = (isset($params['class'])) ? $params['class'] : false;
+        $this->label = (isset($params['label'])) ? $params['label'] : false;
 
 
         $html = "";
-        $html = '<label for="' . $params['name'] . '" class="block text-sm font-medium text-gray-700 mt-px pb-2 dark:text-gray-300">' . $params['label'] . '</label>';
-        $html .= '<select name="' . $params['name'] . '" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500  dark:text-gray-200 dark:bg-gray-900 ease-linear transition-all duration-150" 
+        if ($this->label == true)
+            $html = '<label for="' . $params['name'] . '" class="block text-sm font-medium text-gray-700 mt-px pb-2 dark:text-gray-300">' . $params['label'] . '</label>';
+        $html .= '<select name="' . $params['name'] . '" class="' . $this->class . ' appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 dark:border-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500  dark:text-gray-200 dark:bg-gray-900 ease-linear transition-all duration-150" 
         ' . $this->xOnClick . ' ' . $this->xChange . ' ' . $this->hxGet . ' ' . $this->hxTarget . '  ' . $this->hxInclude . '  ' . $this->hxTrigger . '  ' . $this->hxSwap . ' >';
         $i = 0;
-        $html .= '<option value="0">'.lang('Form.general.choisissezVotreValeur').'</option>';
-        
-        if (isset($params['options']) && count($params['options'])) :
+        $html .= '<option value="0">' . lang('Form.general.choisissezVotreValeur') . '</option>';
 
-            foreach ($params['options'] as $key => $val) :
-                $apinejs  = isset($params['alpinejs']) ?  $params['alpinejs'][$i]  : '' ; 
-                $newSelected = ($params['selected'] === (string) $val) ?  ' selected="selected" ' : '';
-                $html .= '<option value="' . $val . '" ' . $apinejs . ' ' .  $newSelected . '>';
+        if (isset($params['options']) && count($params['options'])):
+
+            foreach ($params['options'] as $key => $val):
+                $apinejs = isset($params['alpinejs']) ? $params['alpinejs'][$i] : '';
+                $newSelected = ($params['selected'] === (string) $val) ? ' selected="selected" ' : '';
+                $html .= '<option value="' . $val . '" ' . $apinejs . ' ' . $newSelected . '>';
                 $html .= !isset($params['byKey']) ? $key : $val;
                 $html .= '</option>';
                 $i++;
             endforeach;
         endif;
-        $html .='</select>';
+        $html .= '</select>';
         $html .= $this->getValidation($params);
         return $html;
     }
@@ -79,14 +84,14 @@ class SelectCell
         $html = '';
         if (isset($params['lang']) && $params['lang'] == true) {
 
-            if (service('validation')->hasError('lang.' . service('language')->getLocale() . '.' . uniforme($params['name']))) :
+            if (service('validation')->hasError('lang.' . service('language')->getLocale() . '.' . uniforme($params['name']))):
                 // print_r($params['validation']); exit;
                 $html = '<div class="invalid-feedback block text-red-600">';
                 $html .= service('validation')->getError('lang.' . service('language')->getLocale() . '.' . uniforme($params['name']));
                 $html .= '</div>';
             endif;
         } else {
-            if (service('validation')->hasError(uniforme($params['name']))) :
+            if (service('validation')->hasError(uniforme($params['name']))):
                 $html = '<div class="invalid-feedback block text-red-600">';
                 $html .= service('validation')->getError(uniforme($params['name']));
                 $html .= '</div>';
